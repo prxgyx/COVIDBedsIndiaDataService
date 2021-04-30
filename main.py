0791cd1e36a22e5306031451ee29a5c2450c73fa
pragya-agrawal-x <pragya.agrwal@gmail.com>
2021-04-30 23:32:20 +0530

    Rajasthan same sheet feature added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from states.Pune import Pune
 from states.Telangana import Telangana
 from states.Haryana import Haryana
 from states.Bengaluru import Bengaluru
+from states.Rajasthan import Rajasthan
 
 TamilNadu().push_data()
 
@@ -13,4 +14,8 @@ Haryana().push_data()
 
 Bengaluru().push_data()
 
+<<<<<<< HEAD
 AndhraPradesh().push_data()
+=======
+Rajasthan().push_data()
+>>>>>>> Rajasthan same sheet feature added
