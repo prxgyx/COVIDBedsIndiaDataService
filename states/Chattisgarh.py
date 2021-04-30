8dc84597c96dffc6e7ec93a9f05725d4918b0f81
Shobhit20 <shobhit20897@gmail.com>
2021-04-30 19:58:32 +0530

    added browser close to selenium

--- states/Chattisgarh.py
+++ states/Chattisgarh.py
@@ -6,41 +6,70 @@ import pandas as pd
 from states.State import State
 
 
-class Chattisgarh(State):
+class Telangana(State):
 
 	def __init__(self):
-		self.stein_url = "https://stein.hamaar.cloud/v1/storages/6089833203eef38338d05a73"
-		self.source_url = "https://cg.nic.in/health/covid19/RTPBedAvailable.aspx"
-		self.main_sheet_name = "Chattisgarh"
+		self.state_name = "Telangana"
+		self.stein_url = "https://stein.hamaar.cloud/v1/storages/6089829403eef36d93d05a6f"
+		self.source_url = "http://164.100.112.24/SpringMVC/Hospital_Beds_Statistic_Bulletin_citizen.htm"
+		self.custom_sheet_name = "Sheet16"
+		self.main_sheet_name = "Telangana"
+		super().__init__()
+
 
 	def get_dummy_data(self):
 		dummy_data = [
 			{
 	            "SNO": "1",
-	            "DISTRICT": "BALOD",
-	            "HOSPITAL_NAME": "Livelyhood College Balod",
-	            "CONTACT": "Dr. Indra Kumar Chandrakar 9826105264",
-	            "CATEGORY": "Covid Care Center",
-	            "BEDS_TOTAL": "274",
-	            "OXYGEN_BEDS_TOTAL": "10",
+	            "DISTRICT": "Adilabad",
+	            "HOSPITAL_NAME": "A.D.B. HOSPITALS",
+	            "CONTACT": "8555098068",
+	            "REGULAR_BEDS_TOTAL": "11",
+	            "REGULAR_BEDS_OCCUPIED": "3",
+	            "REGULAR_BEDS_VACANT": "8",
+	            "OXYGEN_BEDS_TOTAL": "5",
+	            "OXYGEN_BEDS_OCCUPIED": "5",
 	            "OXYGEN_BEDS_VACANT": "0",
-	            "NONOXYGEN_BEDS_TOTAL": "264",
-	            "NONOXYGEN_BEDS_VACANT": "65",
-	            "ISOLATION_BEDS_OUTSIDE_HOSPITAL_TOTAL": "0",
-	            "ISOLATION_BEDS_OUTSIDE_HOSPITAL_VACANT": "0",
-	            "HDU_BEDS_TOTAL": "0",
-	            "HDU_BEDS_VACANT": "0",
-	            "ICU_BEDS_TOTAL": "0",
+	            "ICU_BEDS_TOTAL": "3",
+	            "ICU_BEDS_OCCUPIED": "3",
 	            "ICU_BEDS_VACANT": "0",
-	            "VENTILATORS_TOTAL": "0",
-	            "VENTILATORS_VACANT": "0",
-	            "BEDS_VACANT": "65",
-	            "EMPANELLED_IN_AYUSHMAN": "Yes",
-	            "LAST_UPDATED_DATE": "28/04/2021",
-	            "LAST_UPDATED_TIME": "10:37"
+	            "TOTAL": "19",
+	            "OCCUPIED": "11",
+	            "VACANT": "8",
+	            "DATE": "28/04/2021",
+	            "TIME": "6:15:04 PM",
+	            "TYPE": "Private"
         	}
 		]
 		return dummy_data
+
+	def get_items_from_table(self, tds, s_no, district_name, i, type_hospital):
+		
+			
+		json_obj = {
+			"SNO": s_no,
+			"DISTRICT": district_name,
+			"HOSPITAL_NAME": ".".join(tds[2-i].text.split(".")[1:]).strip(),
+			"CONTACT": tds[3-i].text,
+			"REGULAR_BEDS_TOTAL": tds[4-i].text,
+			"REGULAR_BEDS_OCCUPIED": tds[5-i].text,
+			"REGULAR_BEDS_VACANT": tds[6-i].text,
+			"OXYGEN_BEDS_TOTAL": tds[7-i].text,
+			"OXYGEN_BEDS_OCCUPIED": tds[8-i].text,
+			"OXYGEN_BEDS_VACANT": tds[9-i].text,
+			"ICU_BEDS_TOTAL": tds[10-i].text,
+			"ICU_BEDS_OCCUPIED": tds[11-i].text,
+			"ICU_BEDS_VACANT": tds[12-i].text,
+			"TOTAL": tds[13-i].text,
+			"OCCUPIED": tds[14 -i].text,
+			"VACANT": tds[15-i].text,
+			"DATE": tds[16-i].text,
+			"TIME": tds[17-i].text,
+			"TYPE": type_hospital
+		}
+
+		return json_obj
+
 
 	def get_data_from_source(self):
 		output_json =[]
@@ -54,50 +83,65 @@ class Chattisgarh(State):
 		while not page_retrieved and retries < 5:
 			try:
 				browser.get(self.source_url)
+
+				# the element for table takes time to load after page is loaded
+				time.sleep(4)
+				browser.find_elements_by_css_selector("table.table-responsive1 > tbody > tr > td")[1].click()
+
+				all_table_rows = browser.find_elements_by_css_selector("table.table-responsive1 > tbody > tr")
+
+				district_name, s_no = "", 0
+
+				output_json = []
+				for table_row in all_table_rows:
+					tds = table_row.find_elements_by_css_selector('td')
+					if len(tds) == 18:
+						s_no = tds[0].text
+						district_name = tds[1].text
+						i = 0
+					else:
+						i = 2
+
+					output_json.append(self.get_items_from_table(tds, s_no, district_name, i, "Government"))
+
+
+				## private hospital tab switching
+
+				browser.find_element_by_css_selector("input[value='P']").click()
+				button_elements = browser.find_elements_by_css_selector("button[type='submit']")
+				for button_element in button_elements:
+					if button_element.text=="VIEW REPORT":
+						button_element.click()
+						break
+
+
+				time.sleep(20)
+
+				all_table_rows = browser.find_elements_by_css_selector("table.table-responsive1 > tbody > tr")
+
+				district_name, s_no = "", 0
+				for table_row in all_table_rows:
+					tds = table_row.find_elements_by_css_selector('td')
+					if len(tds) == 18:
+						s_no = tds[0].text
+						district_name = tds[1].text
+						i = 0
+					else:
+						i = 2
+
+					output_json.append(self.get_items_from_table(tds, s_no, district_name, i, "Private"))
+
 				page_retrieved = True
-			except:
+			except Exception as e:
+				print(e)
 				print("Page failed to load. Retrying")
 				retries +=1
 				time.sleep(10)
 
 		if retries >=5:
-			return [] 
+			return []
 
-		# Clicking the Show data button
-		browser.find_element_by_css_selector("input[value='Show']").click()
 
-		# extracting all table rows
-		elements = browser.find_elements_by_css_selector("table.table > tbody > tr")
-
-		# removing the header and last total entry from the iteration
-		for element in elements[1:-1]:
-			tds = element.find_elements_by_css_selector('td')
-
-			json_obj = {
-			"SNO": tds[0].text,
-			"DISTRICT": tds[1].text,
-			"HOSPITAL_NAME": tds[2].text,
-			"CONTACT": tds[3].text,
-			"CATEGORY": tds[4].text,
-			"BEDS_TOTAL": tds[5].text,
-			"OXYGEN_BEDS_TOTAL": tds[6].text,
-			"OXYGEN_BEDS_VACANT": tds[7].text,
-			"NONOXYGEN_BEDS_TOTAL": tds[8].text,
-			"NONOXYGEN_BEDS_VACANT": tds[9].text,
-			"ISOLATION_BEDS_OUTSIDE_HOSPITAL_TOTAL": tds[10].text,
-			"ISOLATION_BEDS_OUTSIDE_HOSPITAL_VACANT": tds[11].text,
-			"HDU_BEDS_TOTAL": tds[12].text,
-			"HDU_BEDS_VACANT": tds[13].text,
-			"ICU_BEDS_TOTAL": tds[14].text,
-			"ICU_BEDS_VACANT": tds[15].text,
-			"VENTILATORS_TOTAL": tds[16].text,
-			"VENTILATORS_VACANT": tds[17].text,
-			"BEDS_VACANT": tds[18].text,
-			"EMPANELLED_IN_AYUSHMAN": tds[19].text,
-			"LAST_UPDATED_DATE": tds[20].text,
-			"LAST_UPDATED_TIME": tds[21].text,
-			}
-			output_json.append(json_obj)
 		return output_json
 
 
@@ -107,7 +151,5 @@ class Chattisgarh(State):
 		merged_loc_df["HAS_ICU_BEDS"] = merged_loc_df.apply(lambda row: True 
 												if int(row["ICU_BEDS_TOTAL"]) > 0 else False, axis=1)
 
-		merged_loc_df["HAS_VENTILATORS"] = merged_loc_df.apply(lambda row: True 
-												if int(row["VENTILATORS_TOTAL"]) > 0 else False, axis=1)
 		
 		return merged_loc_df
