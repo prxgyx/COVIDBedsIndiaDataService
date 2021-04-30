3615708c583d7dc9544be7ed6ce1e5faf3abef3a
pxthxk <5724479+pxthxk@users.noreply.github.com>
2021-05-01 00:29:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from states.AndhraPradesh import AndhraPradesh
-from states.TamilNadu import TamilNadu
-from states.Pune import Pune
-from states.Telangana import Telangana
-from states.Haryana import Haryana
 from states.Bengaluru import Bengaluru
+from states.Haryana import Haryana
+from states.Pune import Pune
 from states.Rajasthan import Rajasthan
+from states.TamilNadu import TamilNadu
+from states.Telangana import Telangana
 
-TamilNadu().push_data()
-
-Pune().push_data()
-
-Haryana().push_data()
-
-Bengaluru().push_data()
-
-<<<<<<< HEAD
 AndhraPradesh().push_data()
-=======
+Bengaluru().push_data()
+Haryana().push_data()
+Pune().push_data()
 Rajasthan().push_data()
->>>>>>> Rajasthan same sheet feature added
+TamilNadu().push_data()
